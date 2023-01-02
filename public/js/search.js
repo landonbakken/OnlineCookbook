4634542b84851b4062206278329aec9f36069f30
TheRad1 <18326@verona.k12.wi.us>
2023-01-02 12:37:08 -0600

    got things to load

--- public/js/search.js
+++ public/js/search.js
@@ -3,23 +3,28 @@ const recipeCardContainer = document.querySelector("[data-recipe-cards-container
 const searchInput = document.querySelector("[data-search]")
 
 let recipes = []
+const ingredients = getJsonData("ingredients.json");
+const recipes2 = getJsonData("recipes.json");
+
+async function getJsonData(fileName) {
+	let file = await fetch("/database/" + fileName)
+	let data = await file.json()
+	
+	return data
+}
 
 searchInput.addEventListener("input", e => {
   const value = e.target.value.toLowerCase()
   recipes.forEach(recipe => {
-    const isVisible =
-      recipe.name.toLowerCase().includes(value) ||
-      recipe.email.toLowerCase().includes(value)
+    const isVisible = recipe.name.toLowerCase().includes(value) || recipe.email.toLowerCase().includes(value)
     recipe.element.classList.toggle("hide", !isVisible)
   })
 })
 
 fetch("https://jsonplaceholder.typicode.com/users")
-//fetch("/database/ingredients.json")()
   .then(res => res.json())
   .then(data => {
     recipes = data.map(recipe => {
-      recipes = data
       const card = recipeCardTemplate.content.cloneNode(true).children[0]
       const header = card.querySelector("[data-header]")
       const body = card.querySelector("[data-body]")
