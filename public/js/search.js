40aff177fe3d3192486e7c016c205f8194b859f6
TheRad1 <18326@verona.k12.wi.us>
2023-01-03 11:57:23 -0600

    added checks, but they arent working yet

--- public/js/search.js
+++ public/js/search.js
@@ -4,23 +4,60 @@ const searchInput = document.querySelector("[data-search]")
 
 let recipes = []
 let ingredients = []
+let meals = []
+let allInfo = []
+let cards = []
+
+let showIngredients = true
+let showMeals = true
+let showRecipes = true
 
 function updateSettings(setting){
-  console.log(setting)
+  let checked = document.getElementById(setting).checked
+
+  if(setting == "ingredients"){
+    showIngredients = checked
+  }
+  if(setting == "meals"){
+    showMeals = checked
+  }
+  if(setting == "recipes"){
+    showRecipes = checked
+  }
+  updateList()
 }
 async function getJsonData() {
-	let file = await fetch("/database/ingredients")
+  let file = await fetch("/database/ingredients")
 	ingredients = await file.json()
-  file = await fetch("/database/ingredients") //CHANGE THIS TO RECIPES WHEN JSON IS BIGGER
+  file = await fetch("/database/recipes") //CHANGE THIS TO RECIPES WHEN JSON IS BIGGER
   recipes = await file.json()
 }
 
 searchInput.addEventListener("input", e => {
   const value = e.target.value.toLowerCase().split(" ")
-  
-  for(let recipe in recipes){
-    let data = recipes[recipe]
-    let div = document.getElementById(recipe)
+  showOrHide(ingredients, value)
+  showOrHide(recipes, value)
+  updateList()
+})
+
+function updateList(){
+  showHide(ingredients, showIngredients)
+  showHide(recipes, showRecipes)
+}
+
+function showHide(dataList, show){
+  if(!show){
+    for(let dataID in dataList){
+      let div = document.getElementById(dataID)
+      div.classList.add("hide")
+    }
+  }
+}
+
+function showOrHide(dataList, value){
+  for(let dataID in dataList){
+    let data = dataList[dataID]
+    let div = document.getElementById(dataID)
     let show = true
 
     if(!(value.length == 1 && value[0] == "")){
@@ -43,7 +80,7 @@ searchInput.addEventListener("input", e => {
     }
     //data.element.classList.toggle("hide", !isVisible)
   }
-})
+}
 
 //fetch("https://jsonplaceholder.typicode.com/users")
 getJsonData().then(() => {
@@ -55,6 +92,17 @@ getJsonData().then(() => {
     card.id = recipe
     header.textContent = data["displayName"]
     header.href = "/recipe/" + recipe
+    body.textContent = data["type"]
+    recipeCardContainer.append(card)
+  }
+  for(let ingredient in ingredients){ //CHANGE INGREDIENTS TO RECIPES!!!!!!!!
+    let data = ingredients[ingredient]
+    const card = recipeCardTemplate.content.cloneNode(true).children[0]
+    const header = card.querySelector("[data-header]")
+    const body = card.querySelector("[data-body]")
+    card.id = ingredient
+    header.textContent = data["displayName"]
+    header.href = "/ingredient/" + ingredient
     body.textContent = data["type"]
     recipeCardContainer.append(card)
   }
